db13d9c0b79a086bc1efa52b4bff9dd8d334b995
Carlos <gartestc@gmail.com>
2024-04-16 14:05:15 -0400

    Added navbar + meli colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,32 @@
+import "@/styles/global.scss";
+import ProximaNova from 'next/font/local'
 import NavBar from "./components/navbar";
 import Breadcrumb from "./components/breadcrumb";
+
+const proximanova = ProximaNova({
+  src: [
+    {
+      path: './fonts/proximanova-light.woff2',
+      weight: '200',
+    },
+    {
+      path: './fonts/proximanova-regular.woff2',
+      weight: '400',
+    },
+    {
+      path: './fonts/proximanova-semibold.woff2',
+      weight: '700',
+    }
+  ]
+});
+
+export const metadata = {
+  title: "Mercado Libre - Test app",
+  description: "A sample app",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,11 +34,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={proximanova.className}>
       <body>
-        <NavBar />
-        <Breadcrumb />
-        {children}
+          <NavBar />
+          <div className="container">
+          <Breadcrumb />
+            {children}
+          </div>
         </body>
     </html>
   );
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Classic next routing</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
